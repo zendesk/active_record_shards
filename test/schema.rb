bb8f0ab540435a6866d5165b08ca1d0b4ee40f80
Ben Osheroff <ben@zendesk.com>
2011-08-31 09:12:28 -0700

    silence schema creation msgs

--- test/schema.rb
+++ test/schema.rb
@@ -1,24 +1,24 @@
 ActiveRecord::Schema.define(:version => 1) do
+  suppress_messages do
+    create_table "accounts", :force => true do |t|
+      t.string   "name"
+      t.datetime "created_at"
+      t.datetime "updated_at"
+    end
 
-  create_table "accounts", :force => true do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    create_table "emails", :force => true do |t|
+      t.string   "from"
+      t.string   "to"
+      t.text     "mail"
+      t.datetime "created_at"
+      t.datetime "updated_at"
+    end
+
+    create_table "tickets", :force => true do |t|
+      t.string   "title"
+      t.integer  "account_id"
+      t.datetime "created_at"
+      t.datetime "updated_at"
+    end
   end
-
-  create_table "emails", :force => true do |t|
-    t.string   "from"
-    t.string   "to"
-    t.text     "mail"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "tickets", :force => true do |t|
-    t.string   "title"
-    t.integer  "account_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
 end
