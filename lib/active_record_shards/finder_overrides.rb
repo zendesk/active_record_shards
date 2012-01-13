ed563f8937e964d998db9369e01fa755e4c66753
Ben Osheroff <ben@zendesk.com>
2012-01-12 17:45:50 -0800

    more towards avoiding connecting to the master

--- lib/active_record_shards/finder_overrides.rb
+++ lib/active_record_shards/finder_overrides.rb
@@ -1,8 +1,9 @@
 module ActiveRecordShards
   module FinderOverrides
-    SLAVE_METHODS = [ :find_by_sql, :count_by_sql, :calculate ]
+    CLASS_SLAVE_METHODS = [ :find_by_sql, :count_by_sql, :calculate, :find_one, :find_some, :find_every, :quote_value ]
+
     def self.extended(base)
-      SLAVE_METHODS.each do |slave_method|
+      CLASS_SLAVE_METHODS.each do |slave_method|
         base.class_eval <<-EOF, __FILE__, __LINE__ + 1
           class <<self
             def #{slave_method}_with_slave_by_default(*args, &block)
@@ -15,11 +16,34 @@ module ActiveRecordShards
           end
         EOF
       end
+
+      base.class_eval do
+        # fix ActiveRecord to do the right thing, and use our aliased quote_value
+        def quote_value(*args, &block)
+          self.class.quote_value(*args, &block)
+        end
+
+        class << self
+          def transaction_with_slave_off(*args, &block)
+            if on_slave_by_default?
+              old_val = Thread.current[:_active_record_shards_slave_off]
+              Thread.current[:_active_record_shards_slave_off] = true
+            end
+
+            transaction_without_slave_off(*args, &block)
+          ensure
+            if on_slave_by_default?
+              Thread.current[:_active_record_shards_slave_off] = old_val
+            end
+          end
+
+          alias_method_chain :transaction, :slave_off
+        end
+      end
     end
 
-    private
     def on_slave_unless_tx(&block)
-      if on_slave_by_default? && on_master.connection.open_transactions.zero?
+      if on_slave_by_default? && !Thread.current[:_active_record_shards_slave_off]
         on_slave { yield }
       else
         yield
