4c531e8abd1cd83fb1c2fb9364d7378243cb2ebd
Michael Krogh <mkrogh@zendesk.com>
2012-01-24 10:11:36 +0100

    Initial changes  needed for supporting rails 3.2

--- lib/active_record_shards.rb
+++ lib/active_record_shards.rb
@@ -7,11 +7,19 @@ require 'active_record_shards/connection_switcher'
 require 'active_record_shards/association_collection_connection_selection'
 require 'active_record_shards/connection_pool'
 require 'active_record_shards/migration'
+if ActiveRecord::VERSION::MAJOR >= 3 && ActiveRecord::VERSION::MINOR >= 2
+  require 'active_record_shards/connection_specification'
+end
 
 ActiveRecord::Base.extend(ActiveRecordShards::ConfigurationParser)
 ActiveRecord::Base.extend(ActiveRecordShards::Model)
 ActiveRecord::Base.extend(ActiveRecordShards::ConnectionSwitcher)
-ActiveRecord::Associations::AssociationCollection.send(:include, ActiveRecordShards::AssociationCollectionConnectionSelection)
+
+if ActiveRecord::VERSION::MAJOR >= 3 && ActiveRecord::VERSION::MINOR >= 1
+  ActiveRecord::Associations::CollectionProxy.send(:include, ActiveRecordShards::AssociationCollectionConnectionSelection)
+else
+  ActiveRecord::Associations::AssociationCollection.send(:include, ActiveRecordShards::AssociationCollectionConnectionSelection)
+end
 
 module ActiveRecordShards
   def self.rails_env
---
+++ lib/active_record_shards/connection_specification.rb
@@ -0,0 +1,14 @@
+class ActiveRecord::Base
+
+  def self.establish_connection(spec = ENV["DATABASE_URL"])
+    resolver = ConnectionSpecification::Resolver.new spec, configurations
+    spec = resolver.spec
+
+    unless respond_to?(spec.adapter_method)
+      raise AdapterNotFound, "database configuration specifies nonexistent #{spec.config[:adapter]} adapter"
+    end
+
+    remove_connection
+    connection_handler.establish_connection connection_pool_name, spec
+  end
+end
