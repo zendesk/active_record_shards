8a69fcc2b61160ffa1c00dc726f1f998e035bf4e
osheroff <ben@zendesk.com>
2012-03-05 20:19:09 -0800

    a little compatibility layer for rails 2/3

--- lib/active_record_shards/connection_pool.rb
+++ lib/active_record_shards/connection_pool.rb
@@ -2,18 +2,24 @@ ActiveRecord::ConnectionAdapters::ConnectionHandler.class_eval do
   # The only difference here is that we use klass.connection_pool_name
   # instead of klass.name as the pool key
   def retrieve_connection_pool(klass)
-    pool = @class_to_pool[klass.connection_pool_name]
+    pool = connection_pool_hash_compat[klass.connection_pool_name]
     return pool if pool
     return nil if ActiveRecord::Base == klass
     retrieve_connection_pool klass.superclass
   end
 
   def remove_connection(klass)
-    pool = @class_to_pool[klass.connection_pool_name]
-    @class_to_pool.delete_if { |key, value| value == pool }
+    pool = connection_pool_hash_compat[klass.connection_pool_name]
+    connection_pool_hash_compat.delete_if { |key, value| value == pool }
     pool.disconnect! if pool
     pool.spec.config if pool
   end
+
+  def connection_pool_hash_compat
+    @class_to_pool || @connection_pools
+  end
+
+  private :connection_pool_hash_compat
 end
 
 ActiveRecord::Base.singleton_class.class_eval do
