2c09cbbf28a0df18e36ff06a86f6ef3d01ec41e8
Ben Osheroff <ben@zendesk.com>
2014-11-08 12:50:49 -0800

    Add Base.shards for console-convience
    This adds some across-all-shards convience methods:
      Model.shards.find(id)
      Relation.shards.to_a
      Relation.hsards.count

---
+++ lib/active_record_shards/shard_support.rb
@@ -0,0 +1,48 @@
+module ActiveRecordShards
+  class ShardSupport
+    class ShardEnumerator
+      include Enumerable
+
+      def each(&block)
+        ActiveRecord::Base.on_all_shards(&block)
+      end
+    end
+
+    def initialize(scope)
+      @scope = scope
+    end
+
+    def enum
+      ShardEnumerator.new
+    end
+
+    def find(*find_args)
+      ensure_concrete!
+
+      exception = nil
+      enum.each do
+        begin
+          record = @scope.find(*find_args)
+          return record if record
+        rescue ActiveRecord::RecordNotFound => e
+          exception = e
+        end
+      end
+      raise exception
+    end
+
+    def count
+      enum.inject(0) { |accum, shard| @scope.clone.count + accum }
+    end
+
+    def to_a
+      enum.flat_map { @scope.clone.to_a }
+    end
+
+    private
+    def ensure_concrete!
+      raise "Please call this method on a concrete model, not an abstract class!" if @scope.abstract_class?
+    end
+  end
+end
+
