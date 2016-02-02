402f12f4d726f07e66db2127727c86767ed92277
Steven Davidovitz <sdavidovitz@zendesk.com>
2016-02-02 10:55:22 -0800

    add rails_env back to the shard selection key

--- lib/active_record_shards/shard_selection.rb
+++ lib/active_record_shards/shard_selection.rb
@@ -32,10 +32,13 @@ module ActiveRecordShards
     def shard_name(klass = nil, try_slave = true)
       the_shard = shard(klass)
 
-      @shard_names ||= {}
-      @shard_names[the_shard] ||= {}
-      @shard_names[the_shard][try_slave] ||= {}
-      @shard_names[the_shard][try_slave][@on_slave] ||= begin
+      keys = [ActiveRecordShards.rails_env, the_shard, try_slave]
+
+      hash = keys.inject(@shard_names ||= {}) do |accum, key|
+        accum[key] ||= {}
+      end
+
+      hash[@on_slave] ||= begin
         s = ActiveRecordShards.rails_env.dup
         s << "_shard_#{the_shard}" if the_shard
         s << "_slave"              if @on_slave && try_slave
