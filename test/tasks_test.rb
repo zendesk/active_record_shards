7839a6db31eee791d12f77036d7f33a10fc498bd
Michael Grosser <michael@grosser.it>
2016-07-15 14:31:47 -0700

    only do a short rescue for know errors, everything else shows long error
    makes it easier to debug nasty issues and gets it closer to what rails is doing
    
    https://github.com/rails/rails/blob/028748945b7a3bb15555b90a58905190c601a15b/activerecord/lib/active_record/tasks/database_tasks.rb#L134-L144

--- test/tasks_test.rb
+++ test/tasks_test.rb
@@ -7,6 +7,14 @@ Rake::Application.new.rake_require("active_record/railties/databases")
 require 'active_record_shards/tasks'
 
 describe "Database rake tasks" do
+  def capture_stderr
+    $stderr = StringIO.new
+    yield
+    $stderr.string
+  ensure
+    $stderr = STDERR
+  end
+
   let(:config) { Phenix.load_database_config('test/database_tasks.yml') }
   let(:master_name) { config['test']['database'] }
   let(:slave_name) { config['test']['slave']['database'] }
@@ -52,6 +60,19 @@ describe "Database rake tasks" do
       shard_names.each do |name|
         refute_includes databases, name
       end
+    end
+
+    it "does not fail when db is missing" do
+      rake('db:create')
+      rake('db:drop')
+      show_databases(config).wont_include master_name
+    end
+
+    it "fails loudly when unknown error occurs" do
+      ActiveRecordShards::Tasks.stubs(:root_connection).raises(ArgumentError)
+      out = capture_stderr { rake('db:drop') }
+      out.must_include "Couldn't drop "
+      out.must_include "test/helper.rb"
     end
   end
 end
