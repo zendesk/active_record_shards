c2c08723a95cc67b3536d3ac57cc8a5e9bbdc4f6
Igor Justino <igor@zendesk.com>
2017-06-12 18:06:07 +0000

    Set primary key to 'id'
    Fixes UnknownPrimaryKey exceptions.
    
    In a shared connection, ShardedModel.table_exists? will run on the first
    shard (because of ConnectionSwitcher#table_exists_with_default_shard), while
    ShardedModel.connection.table_exists?(sharded_table) will still run on the
    *shared* connection (and will return false, as the sharded table doesn't exist
    in the shared database). Among other potential issues, this behavior causes
    ActiveRecord to cache the sharded table primary_id as nil.
    
    Removing the with_default_shard behavior might cause issues, so instead
    we set primary_key as 'id'. Any model with a different primary key will
    need to explicit set it in the class definition.

--- lib/active_record_shards/model.rb
+++ lib/active_record_shards/model.rb
@@ -53,7 +53,25 @@ module ActiveRecordShards
 
     def self.extended(base)
       base.send(:include, InstanceMethods)
+      base.singleton_class.send(:alias_method, :primary_key_without_default_value, :primary_key)
+      base.singleton_class.send(:alias_method, :primary_key, :primary_key_with_default_value)
       base.after_initialize :initialize_shard_and_slave
+    end
+
+
+    # In a shared connection, ShardedModel.table_exists? will run on the first
+    # shard (because of ConnectionSwitcher#table_exists_with_default_shard), while
+    # ShardedModel.connection.table_exists?(sharded_table) will still run on the
+    # *shared* connection (and will return false, as the sharded table doesn't exist
+    # in the shared database). Among other potential issues, this behavior causes
+    # ActiveRecord to cache the sharded table primary_id as nil.
+    #
+    # Removing the with_default_shard behavior might cause issues, so instead
+    # we set primary_key as 'id'. Any model with a different primary key will
+    # need to explicit set it in the class definition.
+    def primary_key_with_default_value
+      @primary_key = 'id' unless defined? @primary_key
+      primary_key_without_default_value
     end
   end
 end
