93e3e89396cb1ddf587b0b2e1b2a526b4f345913
Jacob Atzen <jatzen@gmail.com>
2017-10-05 13:03:50 +0200

    Add more checking of available configuration
    If we're missing configuration we raise an exception at the point we discover
    the missing configuration. This is to avoid accidentally calling .shard= with
    nil.

--- lib/active_record_shards/connection_switcher.rb
+++ lib/active_record_shards/connection_switcher.rb
@@ -156,8 +156,11 @@ module ActiveRecordShards
         end
 
         if options.key?(:shard)
-          unless configurations[shard_env]
+          unless config = configurations[shard_env]
             raise "Did not find #{shard_env} in configurations, did you forget to add it to your database config? (configurations: #{configurations.inspect})"
+          end
+          unless config['shard_names'].include?(options[:shard])
+            raise "Did not find shard #{options[:shard]} in configurations"
           end
           current_shard_selection.shard = options[:shard]
         end
