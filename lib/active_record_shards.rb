fde86eadfd9449cb89d6dfe961e613f6eac361aa
Benjamin Quorning <benjamin@quorning.net>
2017-10-07 16:21:10 -0700

    Move common patches into lib/active_record_shards.rb

--- lib/active_record_shards.rb
+++ lib/active_record_shards.rb
@@ -8,6 +8,7 @@ require 'active_record_shards/connection_switcher'
 require 'active_record_shards/association_collection_connection_selection'
 require 'active_record_shards/migration'
 require 'active_record_shards/default_slave_patches'
+require 'active_record_shards/schema_dumper_extension'
 
 module ActiveRecordShards
   def self.rails_env
@@ -33,3 +34,6 @@ when '5.0', '5.1'
 else
   raise "ActiveRecordShards is not compatible with #{ActiveRecord::VERSION::STRING}"
 end
+
+ActiveRecord::Associations::Builder::HasAndBelongsToMany.include(ActiveRecordShards::DefaultSlavePatches::Rails41HasAndBelongsToManyBuilderExtension)
+ActiveRecord::SchemaDumper.prepend(ActiveRecordShards::SchemaDumperExtension)
--- lib/active_record_shards/patches-5-0.rb
+++ lib/active_record_shards/patches-5-0.rb
@@ -1,15 +1,2 @@
 # frozen_string_literal: true
-
-
-
-require 'active_record_shards/schema_dumper_extension'
-
-
-
-
-
-ActiveRecord::Associations::Builder::HasAndBelongsToMany.include(ActiveRecordShards::DefaultSlavePatches::Rails41HasAndBelongsToManyBuilderExtension)
-
-ActiveRecord::SchemaDumper.prepend(ActiveRecordShards::SchemaDumperExtension)
-
 ActiveRecord::InternalMetadata.not_sharded
