b3fc44059fd97d0e08b050e9daf914adb57094a6
Benjamin Quorning <benjamin@quorning.net>
2017-10-11 16:45:50 -0700

    Move the single patch into lib/active_record_shards.rb

--- lib/active_record_shards.rb
+++ lib/active_record_shards.rb
@@ -25,13 +25,7 @@ ActiveRecord::Base.extend(ActiveRecordShards::ConnectionSwitcher)
 ActiveRecord::Base.extend(ActiveRecordShards::DefaultSlavePatches)
 ActiveRecord::Relation.include(ActiveRecordShards::DefaultSlavePatches::ActiveRelationPatches)
 ActiveRecord::Associations::CollectionProxy.include(ActiveRecordShards::AssociationCollectionConnectionSelection)
-
-case "#{ActiveRecord::VERSION::MAJOR}.#{ActiveRecord::VERSION::MINOR}"
-when '5.0', '5.1'
-  require 'active_record_shards/patches-5-0'
-else
-  raise "ActiveRecordShards is not compatible with #{ActiveRecord::VERSION::STRING}"
-end
-
 ActiveRecord::Associations::Builder::HasAndBelongsToMany.include(ActiveRecordShards::DefaultSlavePatches::HasAndBelongsToManyBuilderExtension)
 ActiveRecord::SchemaDumper.prepend(ActiveRecordShards::SchemaDumperExtension)
+
+ActiveRecord::InternalMetadata.not_sharded
