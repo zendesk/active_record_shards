d4406b938a84ba3b43e8cd24bc5567245ce60e95
Jacob Atzen <jatzen@gmail.com>
2017-11-06 12:48:58 +0100

    Move connection resolution
    The connection resolution logic is shared between the sharding and the
    master/slave modules so it does not belong in either. Instead we move it up to
    the BaseConfig module.

--- lib/active_record_shards/base_config.rb
+++ lib/active_record_shards/base_config.rb
@@ -3,5 +3,24 @@ module ActiveRecordShards
     def connection_config
       {}
     end
+
+    def connection_specification_name
+      name = connection_resolver.resolve_connection_name(connection_config)
+
+      unless configurations[name] || name == "primary"
+        raise ActiveRecord::AdapterNotSpecified, "No database defined by #{name} in your database config. (configurations: #{configurations.inspect})"
+      end
+
+      name
+    end
+
+    def connection_resolver
+      Thread.current[:connection_resolver] ||=
+        ActiveRecordShards::ConnectionResolver.new(configurations)
+    end
+
+    def reset_connection_resolver
+      Thread.current[:connection_resolver] = nil
+    end
   end
 end
--- lib/active_record_shards/connection_switcher.rb
+++ lib/active_record_shards/connection_switcher.rb
@@ -44,27 +44,8 @@ module ActiveRecordShards
       switch_connection(old_options)
     end
 
-    def connection_specification_name
-      name = connection_resolver.resolve_connection_name(connection_config)
-
-      unless configurations[name] || name == "primary"
-        raise ActiveRecord::AdapterNotSpecified, "No database defined by #{name} in your database config. (configurations: #{configurations.inspect})"
-      end
-
-      name
-    end
-
     def connection_config
       super.merge(current_shard_selection.options.merge(sharded: is_sharded?))
-    end
-
-    def connection_resolver
-      Thread.current[:connection_resolver] ||=
-        ActiveRecordShards::ConnectionResolver.new(configurations)
-    end
-
-    def reset_connection_resolver
-      Thread.current[:connection_resolver] = nil
     end
 
     def supports_sharding?
