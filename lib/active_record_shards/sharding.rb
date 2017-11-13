8e29b8cf5750d72bd5a79a30e0f30de065509686
Jacob Atzen <jatzen@gmail.com>
2017-11-13 13:11:44 +0100

    Rename files to better reflect their purpose

--- lib/active_record_shards/sharding.rb
+++ lib/active_record_shards/sharding.rb
@@ -123,9 +123,9 @@ end
 
 case "#{ActiveRecord::VERSION::MAJOR}.#{ActiveRecord::VERSION::MINOR}"
 when '5.0'
-  require 'active_record_shards/connection_switcher_5_0'
+  require 'active_record_shards/db_connection_5_0'
 when '5.1', '5.2'
-  require 'active_record_shards/connection_switcher_5_1'
+  require 'active_record_shards/db_connection_5_1'
 else
   raise "ActiveRecordShards is not compatible with #{ActiveRecord::VERSION::STRING}"
 end
