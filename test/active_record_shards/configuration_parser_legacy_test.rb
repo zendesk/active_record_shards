40a0658cb4040c71e705daaa75269697a63b22ad
Benjamin Quorning <benjamin@quorning.net>
2021-03-18 11:17:23 +0100

    Cleaner ConfigurationParser tests

--- test/active_record_shards/configuration_parser_legacy_test.rb
+++ test/active_record_shards/configuration_parser_legacy_test.rb
@@ -2,126 +2,91 @@
 
 require_relative '../helper'
 
-describe ActiveRecordShards::ConfigurationParser do
-  describe "exploding the database.yml" do
-    let(:yaml) do
-      <<~YAML
-        test:
-          adapter: mysql
-          encoding: utf8
-          database: ars_test
-          port: 123
-          username: root
-          password:
-          host: main_host
-          slave:
-            host: main_replica_host
-          shards:
-            500:
-              database: ars_test_shard_500
-              host: shard_500_host
-              slave:
-                host: shard_500_replica_host
-            501:
-              database: ars_test_shard_501
-              host: shard_501_host
-              slave:
-                database: ars_test_shard_501_replica
-      YAML
-    end
+describe "ActiveRecordShards::ConfigurationParser.explode" do
+  let(:yaml) do
+    <<~YAML
+      test:
+        adapter: mysql
+        encoding: utf8
+        database: ars_test
+        port: 123
+        username: root
+        password:
+        host: main_host
+        slave:
+          host: main_replica_host
+        shards:
+          500:
+            database: ars_test_shard_500
+            host: shard_500_host
+            slave:
+              host: shard_500_replica_host
+          501:
+            database: ars_test_shard_501
+            host: shard_501_host
+            slave:
+              database: ars_test_shard_501_replica
+    YAML
+  end
 
-    before do
-      @exploded_conf = ActiveRecordShards::ConfigurationParser.explode(YAML.safe_load(yaml))
-    end
+  before do
+    @exploded_conf = ActiveRecordShards::ConfigurationParser.explode(YAML.safe_load(yaml))
+  end
 
-    describe "main replica" do
-      before { @conf = @exploded_conf["test_replica"] }
+  it "expands configuration for the main primary" do
+    config = @exploded_conf["test"]
 
-      it "explodes" do
-        @conf["shard_names"] = @conf["shard_names"].to_set
-        assert_equal({
-          "adapter"     => "mysql",
-          "encoding"    => "utf8",
-          "database"    => "ars_test",
-          "port"        => 123,
-          "username"    => "root",
-          "password"    => nil,
-          "host"        => "main_replica_host",
-          "shard_names" => [500, 501].to_set
-        }, @conf)
-      end
-    end
+    shared_assertions(config)
+    assert_equal "main_host", config["host"]
+    assert_equal "ars_test", config["database"]
+  end
 
-    describe "shard a" do
-      describe "primary" do
-        before { @conf = @exploded_conf["test_shard_500"] }
-        it "be exploded" do
-          @conf["shard_names"] = @conf["shard_names"].to_set
-          assert_equal({
-            "adapter"     => "mysql",
-            "encoding"    => "utf8",
-            "database"    => "ars_test_shard_500",
-            "port"        => 123,
-            "username"    => "root",
-            "password"    => nil,
-            "host"        => "shard_500_host",
-            "shard_names" => [500, 501].to_set
-          }, @conf)
-        end
-      end
+  it "expands configuration for the main replica" do
+    config = @exploded_conf["test_replica"]
 
-      describe "replica" do
-        before { @conf = @exploded_conf["test_shard_500_replica"] }
-        it "be exploded" do
-          @conf["shard_names"] = @conf["shard_names"].to_set
-          assert_equal({
-            "adapter"     => "mysql",
-            "encoding"    => "utf8",
-            "database"    => "ars_test_shard_500",
-            "port"        => 123,
-            "username"    => "root",
-            "password"    => nil,
-            "host"        => "shard_500_replica_host",
-            "shard_names" => [500, 501].to_set
-          }, @conf)
-        end
-      end
-    end
+    shared_assertions(config)
+    assert_equal "main_replica_host", config["host"]
+    assert_equal "ars_test", config["database"]
+  end
 
-    describe "shard b" do
-      describe "primary" do
-        before { @conf = @exploded_conf["test_shard_501"] }
-        it "be exploded" do
-          @conf["shard_names"] = @conf["shard_names"].to_set
-          assert_equal({
-            "adapter"     => "mysql",
-            "encoding"    => "utf8",
-            "database"    => "ars_test_shard_501",
-            "port"        => 123,
-            "username"    => "root",
-            "password"    => nil,
-            "host"        => "shard_501_host",
-            "shard_names" => [500, 501].to_set
-          }, @conf)
-        end
-      end
+  it "expands configuration for shard 500's primary" do
+    config = @exploded_conf["test_shard_500"]
 
-      describe "replica" do
-        before { @conf = @exploded_conf["test_shard_501_replica"] }
-        it "be exploded" do
-          @conf["shard_names"] = @conf["shard_names"].to_set
-          assert_equal({
-            "adapter"     => "mysql",
-            "encoding"    => "utf8",
-            "database"    => "ars_test_shard_501_replica",
-            "port"        => 123,
-            "username"    => "root",
-            "password"    => nil,
-            "host"        => "shard_501_host",
-            "shard_names" => [500, 501].to_set
-          }, @conf)
-        end
-      end
-    end
+    shared_assertions(config)
+    assert_equal "shard_500_host", config["host"]
+    assert_equal "ars_test_shard_500", config["database"]
+  end
+
+  it "expands configuration for shard 500's replica" do
+    config = @exploded_conf["test_shard_500_replica"]
+
+    shared_assertions(config)
+    assert_equal "shard_500_replica_host", config["host"]
+    assert_equal "ars_test_shard_500", config["database"]
+  end
+
+  it "expands configuration for shard 501's primary" do
+    config = @exploded_conf["test_shard_501"]
+
+    shared_assertions(config)
+    assert_equal "shard_501_host", config["host"]
+    assert_equal "ars_test_shard_501", config["database"]
+  end
+
+  it "expands configuration for shard 501's replica" do
+    config = @exploded_conf["test_shard_501_replica"]
+
+    shared_assertions(config)
+    assert_equal "shard_501_host", config["host"]
+    assert_equal "ars_test_shard_501_replica", config["database"]
+  end
+
+  def shared_assertions(config)
+    assert_equal "mysql", config["adapter"]
+    assert_equal "utf8", config["encoding"]
+    assert_equal 123, config["port"]
+    assert_equal "root", config["username"]
+    assert_nil config["password"]
+    assert_equal [500, 501], config["shard_names"]
   end
 end
