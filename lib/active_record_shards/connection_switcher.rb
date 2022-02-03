02576f26c177a91fa29d6685fd9849df9561037f
Benjamin Quorning <benjamin@quorning.net>
2022-02-03 12:08:37 +0100

    Memoize Active Record configurations
    Rails 6 changed ActiveRecord configurations from a hash to a custom
    object. You can still access the configurations with Hash syntax but it
    gives deprecation warnings and is a lot slower.
    
    This PR memoizes the configurations so we only have to do the slow
    access once. Needs to be changed to the new syntax (configs_for…) before
    that API is removed.
    
    We updated the performance test with new results showing Rails 6.0 being
    significantly faster.
    
    Co-authored-by: Nony Dutton <nony.dutton@zendesk.com>

--- lib/active_record_shards/connection_switcher.rb
+++ lib/active_record_shards/connection_switcher.rb
@@ -221,8 +221,10 @@ when '4.2'
   require 'active_record_shards/connection_switcher-4-2'
 when '5.0'
   require 'active_record_shards/connection_switcher-5-0'
-when '5.1', '5.2', '6.0'
+when '5.1', '5.2'
   require 'active_record_shards/connection_switcher-5-1'
+when '6.0'
+  require 'active_record_shards/connection_switcher-6-0'
 else
   raise "ActiveRecordShards is not compatible with #{ActiveRecord::VERSION::STRING}"
 end
