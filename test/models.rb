db3d3e32899bddb2fa82b90a9d778d4f37f4e9b5
Benjamin Quorning <benjamin@quorning.net>
2022-04-22 17:32:21 +0200

    6.1 sharding info in tests

--- test/models.rb
+++ test/models.rb
@@ -1,30 +1,41 @@
 # frozen_string_literal: true
 
-class Account < ActiveRecord::Base
+class UnshardedApplicationRecord < ActiveRecord::Base
+  self.abstract_class = true
+
+  connects_to database: { writing: :default, reading: :default_replica }
+end
+
+class ShardedApplicationRecord < ActiveRecord::Base
+  self.abstract_class = true
+
+  connects_to shards: {
+    shard_0: { writing: :shard_0, reading: :shard_0_replica },
+    shard_1: { writing: :shard_1, reading: :shard_1_replica }
+  }
+end
+
+class Account < UnshardedApplicationRecord
   # attributes: id, name, updated_at, created_at
-  not_sharded
 
   has_many :tickets
   has_many :account_things
   has_and_belongs_to_many :people, join_table: 'account_people'
 end
 
-class AccountThing < ActiveRecord::Base
-  not_sharded
-
+class AccountThing < UnshardedApplicationRecord
   scope(:enabled, -> { where(enabled: true) })
 end
 
 class AccountInherited < Account
 end
 
-class Ticket < ActiveRecord::Base
+class Ticket < ShardedApplicationRecord
   # attributes: id, title, account_id, updated_at, created_at
   belongs_to :account
 end
 
-class Person < ActiveRecord::Base
-  not_sharded
+class Person < UnshardedApplicationRecord
 end
 
 class User < Person
