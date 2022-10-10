82d3c3775e7f2fab2f8746fe4725126c1fa5e604
Nony Dutton <nony.dutton@zendesk.com>
2022-10-10 08:31:27 +0200

    Fix Rails 6.1 deprecation warnings
    Uses the new Rails 6.1 configurations methods.
    
    This uses a case check in `connection_switcher.rb` instead of creating a
    separate connection_switcher file for Rails 6.1.
    
    Case checks are *slower* but it may not be significant? Worth a
    conversation.
    
    ```
    \# Running:
    
    Warming up --------------------------------------
               with_case   100.484k i/100ms
            without_case   148.390k i/100ms
    Calculating -------------------------------------
               with_case      1.005M (± 1.2%) i/s -      5.125M in   5.098112s
            without_case      1.485M (± 0.6%) i/s -      7.568M in   5.097460s
    
    Comparison:
            without_case:  1484686.8 i/s
               with_case:  1005350.1 i/s - 1.48x  (± 0.00) slower
    ```

---
+++ lib/active_record_shards/connection_switcher-6-1.rb
@@ -0,0 +1,31 @@
+module ActiveRecordShards
+  module ConnectionSwitcher
+    def connection_specification_name
+      name = current_shard_selection.resolve_connection_name(sharded: is_sharded?, configurations: configurations)
+
+      @_ars_connection_specification_names ||= {}
+      unless @_ars_connection_specification_names.include?(name)
+        unless configurations.configs_for(env_name: name, include_replicas: true).any? || name == "ActiveRecord::Base"
+          raise ActiveRecord::AdapterNotSpecified, "No database defined by #{name} in your database config. (configurations: #{configurations.configurations.map(&:env_name).inspect})"
+        end
+
+        @_ars_connection_specification_names[name] = true
+      end
+
+      name
+    end
+
+    private
+
+    def ensure_shard_connection
+      # See if we've connected before. If not, call `#establish_connection`
+      # so that ActiveRecord can resolve connection_specification_name to an
+      # ARS connection.
+      spec_name = connection_specification_name
+
+      pool = connection_handler.retrieve_connection_pool(spec_name)
+
+      connection_handler.establish_connection(spec_name.to_sym) if pool.nil?
+    end
+  end
+end
